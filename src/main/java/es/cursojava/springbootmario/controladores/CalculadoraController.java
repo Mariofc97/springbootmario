82a5f292aa5bc35694229c737c2fc2d783dfda71
Mario Ferron <mario@email.com>
2026-01-22 20:20:05 +0100

    primer commit

---
+++ src/main/java/es/cursojava/springbootmario/controladores/CalculadoraController.java
@@ -0,0 +1,6 @@
+package es.cursojava.springbootmario.controladores;
+
+public class CalculadoraController {
+
+	
+}
---
+++ src/test/java/es/cursojava/springbootmario/SpringbootmarioApplicationTests.java
@@ -0,0 +1,13 @@
+package es.cursojava.springbootmario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootmarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
